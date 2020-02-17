6dc1c71d9cc1b0346cc1a46c5a17f255e1998532
Atsushi <shimarisu.araiguma@gmail.com>
2020-02-17 22:09:04 +0900

    Sixth Commit

---
+++ src/components/BookingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Booking = () => (
+  <div className="container">
+    <h1>工事中</h1>
+    <Link to="/">Homeに戻る</Link>  
+  </div>
+);
+
+export default Booking;
